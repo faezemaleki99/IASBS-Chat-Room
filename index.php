5b8da6d91b15bff16c5f90bac0e1b9a4d41d0428
faezemaleki99 <74075104+faezemaleki99@users.noreply.github.com>
2020-12-28 00:04:13 +0330

    container class created
    in our home page (first page)

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
+<?php include "view/header.html"; ?>
+<div class="container">
 
+<center><h2 style="color:white; margin-top:20%">Welcome to simple chat system.</h2></center>
+</div>
+
+</body>
+</html>
