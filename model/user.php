693868c4961af399688ad96c6554809e6a5c0417
faezemaleki99 <74075104+faezemaleki99@users.noreply.github.com>
2020-12-27 23:07:01 +0330

    create class person in user.php in model

--- model/user.php
+++ model/user.php
@@ -1,1 +1,39 @@
+<?php
+require_once "database.php";
+abstract class person
+{
+    public $name;
+    public $number;
+    public $address;
+
+    function getName()
+    {
+        return $this->name;
+    }
+
+    function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    function getNumber()
+    {
+        return $this->number;
+    }
+
+    function setNumber($number)
+    {
+        $this->number = $number;
+    }
+
+    function getAddress()
+    {
+        return $this->address;
+    }
+
+    function setAddress($address)
+    {
+        $this->address = $address;
+    }
+}
 
