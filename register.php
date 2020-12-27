cae573ebb8e44109d4f525a86865044809a0a541
faezemaleki99 <74075104+faezemaleki99@users.noreply.github.com>
2020-12-28 00:28:56 +0330

    add to registration form : get name

--- register.php
+++ register.php
@@ -1,1 +1,12 @@
 <?php include "view/header.html"; ?>
+<div class="container">
+
+  <center><h2>Registration form</h2></center></br>
+  <form class="form-horizontal" method="post" action="add_user.php">
+    <div class="form-group">
+      <label class="control-label col-sm-2 col-sm-offset-2" for="name">Name:</label>
+	  
+      <div class="col-sm-5">
+        <input type="text" class="form-control" id="name" placeholder="Enter name" name="name" required>
+      </div>
+    </div>
