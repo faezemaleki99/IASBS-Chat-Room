19ab3b57a2810572e273b317239d82081179dca4
faezemaleki99 <74075104+faezemaleki99@users.noreply.github.com>
2020-12-29 00:44:26 +0330

    Start communication with user's name --  communicate with the chat table in the database

--- home.php
+++ home.php
@@ -1,1 +1,12 @@
+<?php 
+	session_start();
+	if(isset($_SESSION['name']))
+	{
+		include "view/header2.html"; 
+		include "config.php"; 
+		
+		$sql="SELECT * FROM `chat`";
 
+		$query = mysqli_query($conn,$sql);
+
+?>
