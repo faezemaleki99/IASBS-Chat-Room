56578ca9a9186636afc6c9839d1621b46e2e075b
faezemaleki99 <74075104+faezemaleki99@users.noreply.github.com>
2020-12-29 00:48:25 +0330

    Container class to display "welcome" message and open chat button

--- home.php
+++ home.php
@@ -10,3 +10,22 @@
 		$query = mysqli_query($conn,$sql);
 
 ?>
+
+<div class="container">
+  <center><h2>Welcome <span style="color:#dd7ff3;"><?php echo $_SESSION['name']; ?> !</span></h2>
+  <br><br>
+	<label>Click below to Join the chat</label><br>
+	<br><br>
+	<a href="chatpage.php" class="btn btn-primary">Open Chat</a>
+  
+</div>
+	
+</body>
+</html>
+<?php
+	}
+	else
+	{
+		header('location:index.php');
+	}
+?>
