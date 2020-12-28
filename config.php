90dc61e33107e4f8e15504eeffc3c83b03be8135
faezemaleki99 <74075104+faezemaleki99@users.noreply.github.com>
2020-12-28 19:18:25 +0330

    create config.php for specify routes

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+$dbHost ='localhost';
+$dbUsername ='root';
+$dbPassword ='';
+$dbDatabase ='chatroom_db';
+$conn=mysqli_connect($dbHost,$dbEmail,$dbPassword,$dbDatabase);
+
+
+$ViewPath = 'view/';
+
+?>
