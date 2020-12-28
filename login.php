4f45a9733bb19fe3795ea449a20311b454110b38
faezemaleki99 <74075104+faezemaleki99@users.noreply.github.com>
2020-12-28 18:45:27 +0330

    Check user's email and password in the database
    check it in registry table in database, if exist show him/her home page

--- login.php
+++ login.php
@@ -1,1 +1,17 @@
 <?php include "view/header.html"; ?>
+<?php
+  include "config.php";
+  if($_POST)
+	{
+		$email = $_POST['email'];
+		$password = $_POST['password'];
+		
+		$sql = "SELECT * FROM `register` where email = '".$email."' and password = '".$password."' ";
+		$query =  mysqli_query($conn, $sql);
+		if(mysqli_num_rows($query)>0)
+		{
+			$row = mysqli_fetch_assoc($query);
+			session_start();
+			$_SESSION['name'] = $row['name'];
+			header('Location: home.php');
+		}
