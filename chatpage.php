9a297c39ebc7dea4b37397220d3a9f948e2358fe
faezemaleki99 <74075104+faezemaleki99@users.noreply.github.com>
2020-12-29 20:50:10 +0330

    show welcome message with her/him name in container class

--- chatpage.php
+++ chatpage.php
@@ -9,3 +9,9 @@
 
 		$query = mysqli_query($conn,$sql);
 ?>
+
+<div class="container">
+  <center><h2>Welcome <span style="color:#dd7ff3;"><?php echo $_SESSION['name']; ?> !</span></h2>
+	<label>Join the chat</label>
+  </center></br>
+  <div class="display-chat">
