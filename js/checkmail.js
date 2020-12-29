401d0e6141d500d51c6455008c9ff7ce8ced2086
faezemaleki99 <74075104+faezemaleki99@users.noreply.github.com>
2020-12-30 00:33:44 +0330

    Robustness: deal with errors (check with un)

--- js/checkmail.js
+++ js/checkmail.js
@@ -1,1 +1,16 @@
+function checkemail() {
+    username = document.getElementById("uiEmail").value;
+    document.getElementById("uiMessage").innerHTML = 'Please wait...';
 
+    $.ajax({
+        url: 'add-user.php',
+        type: 'POST',
+        async: !1,
+        //contentType: 'charset=utf-8',
+        data: { un: email},
+        success: function (data) {
+            document.getElementById("uiMessage").innerHTML = data;
+        }
+    });
+
+}
