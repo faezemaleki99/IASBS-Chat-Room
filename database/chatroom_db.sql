d8a4371322e174a3743779431918fa30837e5620
faezemaleki99 <74075104+faezemaleki99@users.noreply.github.com>
2021-01-02 20:35:22 +0330

    Add two items in chat table (to and from for delete)

--- database/chatroom_db.sql
+++ database/chatroom_db.sql
@@ -2,6 +2,8 @@ CREATE TABLE IF NOT EXISTS `chat` (
   `id` int(11) NOT NULL,
   `name` varchar(255) DEFAULT NULL,
   `message` text,
+  `from` int(10) unsigned NOT NULL,
+  `to` int(10) unsigned NOT NULL,
   `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
