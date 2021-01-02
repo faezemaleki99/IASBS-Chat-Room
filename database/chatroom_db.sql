c8a4ec3802f2576ae41f152fa9cfda1dba418823
faezemaleki99 <74075104+faezemaleki99@users.noreply.github.com>
2021-01-02 20:14:30 +0330

    Table structure for table `register`

--- database/chatroom_db.sql
+++ database/chatroom_db.sql
@@ -4,3 +4,13 @@ CREATE TABLE IF NOT EXISTS `chat` (
   `message` text,
   `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `register` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `number` varchar(255) NOT NULL,
+  `address` text NOT NULL,
+  `modified_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
