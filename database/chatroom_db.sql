02774d576361da070fde3d612b16ac1324f59d81
faezemaleki99 <74075104+faezemaleki99@users.noreply.github.com>
2021-01-02 20:13:58 +0330

    Table structure for table `chat`

--- database/chatroom_db.sql
+++ database/chatroom_db.sql
@@ -1,1 +1,6 @@
-
+CREATE TABLE IF NOT EXISTS `chat` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `message` text,
+  `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
